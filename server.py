d10f491d6e4a85b5f037d908c0905228fc90b210
beep-grandline <bhshimab@gmail.com>
2026-03-28 16:57:35 -0700

    Update server.py

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, WebSocket
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from dotenv import load_dotenv
+from bot import bot
 
 load_dotenv()
 
@@ -20,13 +21,18 @@ async def ws_test(websocket: WebSocket):
     try:
         while True:
             counter += 1
-            await websocket.send_text(json.dumps({
-                "status": "ok",
-                "ping": counter
-            }))
+            await websocket.send_text(json.dumps({"status": "ok", "ping": counter}))
             await asyncio.sleep(2)
     except:
         pass
 
+async def main():
+    config = uvicorn.Config(app, host="0.0.0.0", port=8000)
+    server = uvicorn.Server(config)
+    await asyncio.gather(
+        server.serve(),
+        bot.start(os.getenv("DISCORD_TOKEN"))
+    )
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    asyncio.run(main())
