61f308ca3a503b8cbc04644407319ec13b1f8de8
beep-grandline <bhshimab@gmail.com>
2026-03-29 14:36:52 -0700

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,12 @@
 import sqlite3
 
+# COORDINATES
+HEX_DIRECTIONS = {
+    "fw":  ( 1, 0),
+    "bw":  (-1, 0),
+    "rf":  ( 0, 1),
+    "rb":  (-1, 1),
+    "lf":  ( 1,-1),
+    "lb":  ( 0,-1),
+}
 
