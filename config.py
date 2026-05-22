6b27a4de74f4754f2e09f0ae857b09cd41a141a4
beep-grandline <bhshimab@gmail.com>
2026-05-21 22:43:34 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import discord
 
-GUILD_ID = 1487526877185704107   # ← your guild ID
+GUILD_ID = 1487526877185704107
 MY_GUILD  = discord.Object(id=GUILD_ID)
 GAME_ADMIN = "Admin"
 GAME_MOD   = "Mod"
