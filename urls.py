1ffb829eac9e4a6fb33ebefae1d90cd4936b8611
beep-grandline <bhshimab@gmail.com>
2026-05-28 09:06:12 -0700

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,10 +1,2 @@
-# ═══════════════════════════════════════════════════════════════════════════════
-#  urls.py  ·  Special image and asset URLs for bot events.
-#  Add your actual URLs here.
-# ═══════════════════════════════════════════════════════════════════════════════
-
-SPECIAL_IMAGES = {
-    "kuma_teleport": "https://media.discordapp.net/attachments/799885743451144205/1509454174448914453/image.png?ex=6a193c3f&is=6a17eabf&hm=84de21517d515efaf94645d9fb13b42c318fccfc31615793d59fa14df7d262b0&=&format=webp&quality=lossless&width=825&height=378",   # paste Kuma teleport image URL here
-}
-
-KUMA_TELEPORT_IMAGE = "https://media.discordapp.net/attachments/799885743451144205/1509454174448914453/image.png?ex=6a193c3f&is=6a17eabf&hm=84de21517d515efaf94645d9fb13b42c318fccfc31615793d59fa14df7d262b0&=&format=webp&quality=lossless&width=825&height=378"
+KUMA_TELEPORT_WARN  = "https://cdn.discordapp.com/attachments/799885743451144205/1509454174448914453/image.png?ex=6a193c3f&is=6a17eabf&hm=84de21517d515efaf94645d9fb13b42c318fccfc31615793d59fa14df7d262b0"
+KUMA_TELEPORT_IMAGE = "https://cdn.discordapp.com/attachments/799885743451144205/1509588467112218634/image.png?ex=6a19b951&is=6a1867d1&hm=04ea42751bc55df0e17db48a79735362b8061d3b2661a047db4dd8ef73d7bcda"
