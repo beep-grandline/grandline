3fe1cab2c01ebdba6cbdbde4ede472515c78b78c
beep-grandline <bhshimab@gmail.com>
2026-05-28 20:05:02 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ from crew_commands import crew_group
 bot.tree.add_command(crew_group)
 from travel_commands import travel_group, setup_travel_task
 bot.tree.add_command(travel_group)
-from spyglass import spyglass_cmd, load_islands
+from spyglass import spyglass_cmd, load_islands, prerender_all_flags
 bot.tree.add_command(spyglass_cmd)
 
 @bot.event
@@ -48,6 +48,7 @@ async def on_ready():
     setup_travel_task(bot)
     load_npcs()
     load_islands()
+    asyncio.create_task(prerender_all_flags())
     bot.add_view(RolePicker())
     bot.tree.clear_commands(guild=None)
     await bot.tree.sync()
